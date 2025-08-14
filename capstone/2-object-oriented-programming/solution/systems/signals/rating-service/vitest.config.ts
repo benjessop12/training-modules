3a5d430401317d6069eb5c9cda6a4dab8dfe8032
benjessop12 <56115861+benjessop12@users.noreply.github.com>
2025-08-14 12:19:16 +1200

    chore: Check in OOP modules and preliminary capstone project for supporting session (#10)

---
+++ capstone/2-object-oriented-programming/solution/systems/signals/rating-service/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    includeSource: ['src/**/*.ts'],
+    include: ['src/**/*.test.ts'],
+    globals: true,
+    passWithNoTests: true,
+    silent: false,
+    reporters: ['basic', 'junit'],
+    outputFile: {
+      junit: './TestResult.xml'
+    },
+    coverage: {
+      reporter: ['cobertura', 'text'],
+      provider: 'istanbul',
+    }
+  }
+})
